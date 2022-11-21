c064a031f3287875e210f323ca3ce0c15a8205ab
Jean-Castadere-nexxtsoft <Jean.Castadere@nexxtsoft.com>
2022-11-21 10:03:37 +0100

    add project

---
+++ STW/Y09.Energy.Gateway.Stw.PurtelApi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Y09.Energy.Gateway.Stw.PurtelApi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
