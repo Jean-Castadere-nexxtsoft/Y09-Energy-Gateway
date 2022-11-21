7df92c19a7b6a3b00d03d5b5b14b8245421e457e
Jean-Castadere-nexxtsoft <Jean.Castadere@nexxtsoft.com>
2022-11-21 14:41:40 +0100

    add dto model

--- STW/Y09.Energy.Gateway.Stw.PurtelApi/Dtos/CreateContractJson.cs
+++ STW/Y09.Energy.Gateway.Stw.PurtelApi/Dtos/CreateContractJson.cs
@@ -6,7 +6,43 @@ using System.Threading.Tasks;
 
 namespace Y09.Energy.Gateway.Stw.PurtelApi.Dtos
 {
+    /// <summary>
+    /// create contract model to json
+    /// </summary>
     internal class CreateContractJson
     {
+
+        //Required
+        public int anrede { get; set; }
+        public string vorname { get; set; }
+        public string nachname { get; set; }
+        public string adresse { get; set; }
+        public string plz { get; set; }
+        public string ort { get; set; }
+        public int land { get; set; }
+        public string kundennummer_extern { get; set; }
+        public string produkt { get; set; }
+
+        //Optional
+        public string firma { get; set; }
+        public string geburtsdatum { get; set; }
+        public string haus_nr { get; set; }
+        public string zusatz { get; set; }
+        public string ot { get; set; }
+        
+        public int max_channels { get; set; }
+        public int max_channels_vertrag { get; set; }
+        public int max_numbers { get; set; }
+        public int max_numbers_vertrag { get; set; }
+        public int max_fax { get; set; }
+        public int max_fax_vertrag { get; set; }
+        public int max_accounts_vertrag { get; set; }
+        public int ip_eingeschraenkt { get; set; }
+        public int skip_timeout { get; set; }
+        public int billing { get; set; }
+
+
+
+
     }
 }
